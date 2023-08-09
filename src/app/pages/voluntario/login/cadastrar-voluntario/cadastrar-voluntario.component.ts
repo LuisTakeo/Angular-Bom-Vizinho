336d984499c8e790ea575556bdfc9ed68ebc1dd6
LuisTakeo <luistakeo@hotmail.com>
2023-08-08 22:25:23 -0300

    Arrumado navegação das páginas.

--- src/app/pages/voluntario/login/cadastrar-voluntario/cadastrar-voluntario.component.ts
+++ src/app/pages/voluntario/login/cadastrar-voluntario/cadastrar-voluntario.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastrar-voluntario',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./cadastrar-voluntario.component.scss']
 })
 export class CadastrarVoluntarioComponent {
+  novoVoluntario = new FormGroup({
+    nome: new FormControl(''),
+    sobrenome: new FormControl(''),
+    email: new FormControl(''),
+    celular: new FormControl(''),
+    dataNascimento: new FormControl(''),
+    endereco: new FormControl(''),
+    cep: new FormControl(''),
+    senha: new FormControl(''),
+    senhaConfirm: new FormControl(''),
+  })
 
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.novoVoluntario.value);
+  }
 }
